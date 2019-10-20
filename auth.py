213edc2d544e417905a186fa4b81f61dc5d5492f
tawfiqsiam <toto.siam999@gmail.com>
2019-10-20 14:33:40 +0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,5 @@
+id = '[634116842235101186]'
+secret = '[u_ukHzpVUj_0e1scV96bvq1h9lofoQxF]'
+secretkey = "[A random string of chars]"
+redirect = '"https://tawfiqsiam.github.io/Nekool/#/login'
+scopes = 'https://discordapp.com/api/oauth2/authorize?client_id=634116842235101186&redirect_uri=https%3A%2F%2Ftawfiqsiam.github.io%2FNekool%2F%23%2Flogin&response_type=code&scope=identify%20email%20guilds'
